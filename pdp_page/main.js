83e76bb916c54623465710295aff03a183e2bbfd
Sonalkri09 <ninthjune415@gmail.com>
2018-07-24 17:24:28 +0530

    added modal format

--- pdp_page/main.js
+++ pdp_page/main.js
@@ -1,14 +1,3 @@
-/*$(document).ready(function() {
-    $('.add-to-heart').toggle(
-    	function() {
-        $(this)
-        .css('background','url(https://www.carters.com/on/demandware.static/Sites-Carters-Site/-/default/v1528743962165/images/mobileSprite.svg) -222px -71px no-repeat');
-        },
-        function () {
-        $(this)
-        .css('background','url(https://www.carters.com/on/demandware.static/Sites-Carters-Site/-/default/v1528743962165/images/mobileSprite.svg) -268px -71px no-repeat');
-    });
-});*/
 $(document).ready(function() {
 	$('.add-to-heart').click(function(){
 		$(this).toggleClass("main");
