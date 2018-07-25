66178749a1d8757d780748d25b2c35e70a296e47
Sonalkri09 <ninthjune415@gmail.com>
2018-07-25 17:12:11 +0530

    made dialog-box template

--- dialog_box/main.js
+++ dialog_box/main.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-	
-})
+	var dialog, form,
+	zipcode = $("#zipcode"),
+	allFields = $([]).add(zipcode);
+	$("#loc").button().on("click", function() {
+		dialog.dialog("open");
+	});
+
+	dialog = $("#dialog-form").dialog({
+		autoOpen: false,
+		height:400,
+		width: 350,
+		modal: true,
+		buttons: {
+			"Track": googlemap,
+			Cancel: function() {
+				dialog.dialog("close");
+			}
+		},
+		close: function() {
+			form[0].reset();
+			allFields.removeClass("ui-state-error");
+		}
+	});
+	form = dialog.find("form").on("submit", function(event){
+		event.preventDefault();
+		googlemap();
+	});
+
+	function googlemap(){
+		
+		return true;
+	}
+
+});
